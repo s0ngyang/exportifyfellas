28d259e688032ca7cd7137e92245df731adceccc
song yang <keesongyang4@gmail.com>
2025-01-25 23:25:18 +0800

    Use created client id

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,95 +1,101 @@
-import React from "react"
-import i18n from "i18n/config"
-import { render, screen } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
-import App from "./App"
+import React from "react";
+import i18n from "i18n/config";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+import { CLIENT_ID } from "./constants";
 
-const { location } = window
+const { location } = window;
 
 beforeAll(() => {
   // @ts-ignore
-  delete window.location
-})
+  delete window.location;
+});
 
 afterAll(() => {
-  window.location = location
-})
+  window.location = location;
+});
 
 beforeAll(() => {
   // @ts-ignore
-  window.location = { hash: "" }
-})
+  window.location = { hash: "" };
+});
 
 beforeEach(() => {
-  i18n.changeLanguage("en")
-})
+  i18n.changeLanguage("en");
+});
 
 describe("i18n", () => {
   test("language can be changed to French", async () => {
-    render(<App />)
+    render(<App />);
 
-    const linkElement = screen.getByText(/Get Started/i)
-    expect(linkElement).toHaveTextContent("Get Started")
+    const linkElement = screen.getByText(/Get Started/i);
+    expect(linkElement).toHaveTextContent("Get Started");
 
-    const changeLanguageButton = screen.getByTitle(/Change language/i).getElementsByTagName("button")[0]
-    await userEvent.click(changeLanguageButton)
+    const changeLanguageButton = screen
+      .getByTitle(/Change language/i)
+      .getElementsByTagName("button")[0];
+    await userEvent.click(changeLanguageButton);
 
-    const frenchLanguageElement = screen.getByText(/Français/i)
-    expect(frenchLanguageElement).toBeInTheDocument()
+    const frenchLanguageElement = screen.getByText(/Français/i);
+    expect(frenchLanguageElement).toBeInTheDocument();
 
-    await userEvent.click(frenchLanguageElement)
+    await userEvent.click(frenchLanguageElement);
 
-    expect(screen.getByText(/Commencer/)).toBeInTheDocument()
-    expect(linkElement).toHaveTextContent("Commencer")
-  })
-})
+    expect(screen.getByText(/Commencer/)).toBeInTheDocument();
+    expect(linkElement).toHaveTextContent("Commencer");
+  });
+});
 
 describe("logging in", () => {
   test("renders get started button and redirects to Spotify with correct scopes", async () => {
-    render(<App />)
+    render(<App />);
 
-    const linkElement = screen.getByText(/Get Started/i)
+    const linkElement = screen.getByText(/Get Started/i);
 
-    expect(linkElement).toBeInTheDocument()
+    expect(linkElement).toBeInTheDocument();
 
-    await userEvent.click(linkElement)
+    await userEvent.click(linkElement);
 
     expect(window.location.href).toBe(
-      "https://accounts.spotify.com/authorize?client_id=9950ac751e34487dbbe027c4fd7f8e99&redirect_uri=%2F%2F&scope=playlist-read-private%20playlist-read-collaborative%20user-library-read&response_type=token&show_dialog=false"
-    )
-  })
+      `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=%2F%2F&scope=playlist-read-private%20playlist-read-collaborative%20user-library-read&response_type=token&show_dialog=false`
+    );
+  });
 
   describe("post-login state", () => {
     beforeAll(() => {
       // @ts-ignore
-      window.location = { hash: "#access_token=TEST_ACCESS_TOKEN" }
-    })
+      window.location = { hash: "#access_token=TEST_ACCESS_TOKEN" };
+    });
 
     test("renders playlist component on return from Spotify with auth token", () => {
-      render(<App />)
+      render(<App />);
 
-      expect(screen.getByTestId('playlistTableSpinner')).toBeInTheDocument()
-    })
-  })
-})
+      expect(screen.getByTestId("playlistTableSpinner")).toBeInTheDocument();
+    });
+  });
+});
 
 describe("logging out", () => {
   beforeAll(() => {
     // @ts-ignore
-    window.location = { hash: "#access_token=TEST_ACCESS_TOKEN", href: "https://www.example.com/#access_token=TEST_ACCESS_TOKEN" }
-  })
+    window.location = {
+      hash: "#access_token=TEST_ACCESS_TOKEN",
+      href: "https://www.example.com/#access_token=TEST_ACCESS_TOKEN",
+    };
+  });
 
   test("redirects user to login screen which will force a permission request", async () => {
-    const { rerender } = render(<App />)
+    const { rerender } = render(<App />);
 
-    const changeUserElement = screen.getByTitle("Change user")
+    const changeUserElement = screen.getByTitle("Change user");
 
-    expect(changeUserElement).toBeInTheDocument()
+    expect(changeUserElement).toBeInTheDocument();
 
-    await userEvent.click(changeUserElement)
+    await userEvent.click(changeUserElement);
 
-    expect(window.location.href).toBe("https://www.example.com/?change_user=true")
-
-
-  })
-})
+    expect(window.location.href).toBe(
+      "https://www.example.com/?change_user=true"
+    );
+  });
+});
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,34 +1,53 @@
-import React from "react"
-import { withTranslation, WithTranslation } from "react-i18next"
-import { Button } from "react-bootstrap"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { getQueryParam } from "helpers"
+import React from "react";
+import { withTranslation, WithTranslation } from "react-i18next";
+import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getQueryParam } from "helpers";
+import { CLIENT_ID } from "../constants";
 
 class Login extends React.Component<WithTranslation> {
   authorize() {
-    let clientId = getQueryParam("app_client_id")
-    let changeUser = getQueryParam("change_user") !== ""
+    let clientId = getQueryParam("app_client_id");
+    let changeUser = getQueryParam("change_user") !== "";
 
     // Use Exportify application clientId if none given
-    if (clientId === '') {
-      clientId = "9950ac751e34487dbbe027c4fd7f8e99"
+    if (clientId === "") {
+      clientId = CLIENT_ID;
     }
 
-    window.location.href = "https://accounts.spotify.com/authorize" +
-      "?client_id=" + clientId +
-      "&redirect_uri=" + encodeURIComponent([window.location.protocol, '//', window.location.host, window.location.pathname].join('')) +
+    window.location.href =
+      "https://accounts.spotify.com/authorize" +
+      "?client_id=" +
+      clientId +
+      "&redirect_uri=" +
+      encodeURIComponent(
+        [
+          window.location.protocol,
+          "//",
+          window.location.host,
+          window.location.pathname,
+        ].join("")
+      ) +
       "&scope=playlist-read-private%20playlist-read-collaborative%20user-library-read" +
       "&response_type=token" +
-      "&show_dialog=" + changeUser;
+      "&show_dialog=" +
+      changeUser;
   }
 
   render() {
     return (
-      <Button id="loginButton" type="submit" variant="outline-secondary" size="lg" onClick={this.authorize}>
-        <FontAwesomeIcon icon={['far', 'check-circle']} size="sm" /> {this.props.i18n.t("get_started")}
+      <Button
+        id="loginButton"
+        type="submit"
+        variant="outline-secondary"
+        size="lg"
+        onClick={this.authorize}
+      >
+        <FontAwesomeIcon icon={["far", "check-circle"]} size="sm" />{" "}
+        {this.props.i18n.t("get_started")}
       </Button>
-    )
+    );
   }
 }
 
-export default withTranslation()(Login)
+export default withTranslation()(Login);
